f42533826b1a8d8aa08669e5f28c3eb1ce1b0c4f
Gusts Linkevics <gusts@gst.lv>
2024-11-09 11:23:09 +0200

    WPF and DataGrid

---
+++ Lab3.WpfApplication/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Lab2.DataAccess;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab3.WpfApplication.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject, INotifyPropertyChanged
+    {
+        private StudentDbContext _db;
+
+        public MainWindowViewModel()
+        {
+            _db = new StudentDbContext();
+            SelectStudentCommand = new RelayCommand(LoadGrades);
+        }
+
+        private Student[] _students;
+
+        public Student[] Students => _students;
+
+        public void Load()
+        {
+            _students = _db.Students.ToArray();
+        }
+
+        private List<Grade> _grades = new List<Grade>();
+
+        public List<Grade> Grades
+        {
+            get
+            {
+                return _grades;
+            }
+            set
+            {
+                _grades = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Student SelectedStudent { get; set; }
+            
+        public void LoadGrades()
+        {
+            if (SelectedStudent == null)
+            {
+                return;
+            }
+
+            Grades = _db.Grades.Where(s => s.Id == SelectedStudent.Id).ToList();
+        }
+
+        public ICommand SelectStudentCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Lab3.WpfApplication/Window2.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Lab3.WpfApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab3.WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        private MainWindowViewModel _viewModel;
+
+        public Window2()
+        {
+            InitializeComponent();
+            InitModel();
+        }
+
+        private void InitModel()
+        {
+            _viewModel = new MainWindowViewModel();
+
+            _viewModel.Load();
+
+            this.DataContext = _viewModel;
+        }
+    }
+}

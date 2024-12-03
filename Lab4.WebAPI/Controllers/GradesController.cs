7877d21c7af0a93e8f30f5cc9869ee839f4113b6
Gusts Linkevics <gusts@gst.lv>
2024-12-03 19:20:56 +0200

    API extended

---
+++ Lab4.WebAPI/Controllers/GradesController.cs
@@ -0,0 +1,26 @@
+﻿using Lab2.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.WebAPI.Controllers
+{
+    [Route("api/Students")]
+    [ApiController]
+    public class GradesController : ControllerBase
+    {
+        private StudentDbContext _db;
+        public GradesController()
+        {
+            _db = new StudentDbContext();
+        }
+
+        //students/1/grades
+        [HttpGet()]
+        [Route("{id}/Grades")]
+        public Grade[] GetStudents(int id)
+        {
+            var data = _db.Grades.Where(g => g.Student.Id == id).ToArray();
+            return data;
+        }
+    }
+}
